6fbac6cfd2a68a244ec9a2314bd37f1039ebeca0
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-08-25 02:24:47 -0700

    Added an ending, and a comment where the you can change your own questions/answers

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ import string
 print("Welcome to the quiz!")
 
 playing = input("Would you want to play? (Y/N): ")
-Count = 0
 
 def game_loop(question_Count):
-
-    questions = ['Question 1', 'Question 2']
-    answers = ['Answer 1', 'Answer 2']
+    #Add your questions and answers here
+    questions = ['What language is this written in?', 'How many questions are in this?']
+    answers = ['Python', '2']
 
     current_Answer = input(questions[question_Count] + ": ")
     if current_Answer == answers[question_Count]:
         print("Correct!")
         question_Count += 1
         print("Moving on to the next question ")
-        print(question_Count)
+        if question_Count >= len(questions):
+            print("Youve completed the Quiz!")
+            quit()
         game_loop(question_Count)
     elif current_Answer == "q" or current_Answer == "quit":
         print("Come back soon!")
@@ -24,13 +25,16 @@ def game_loop(question_Count):
     else:
         print("That was not correct! Sorry!")
         question_Count += 1
+        if question_Count >= len(questions):
+            print("Youve completed the Quiz!")
+            quit()
         game_loop(question_Count)
-    
-        
+
+
 
 if playing != "y":
     print("Maybe next time!")
     quit()
 else:
     print("Let's Start!")
-    game_loop(Count)
+    game_loop(0)
